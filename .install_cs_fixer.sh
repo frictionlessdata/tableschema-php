9edf99588dd1972010d07c7cfb13391f1be10e5c
Courtney Miles <courtney@milesasylum.com>
2022-01-09 07:55:32 +1000

    Upgrade PHP CS Fixer and change rules to allow existing code to pass

--- .install_cs_fixer.sh
+++ .install_cs_fixer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if [ ! -f ./php-cs-fixer ]; then
-    wget https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v2.3.2/php-cs-fixer.phar -O php-cs-fixer
+    wget https://cs.symfony.com/download/php-cs-fixer-v3.phar -O php-cs-fixer
     chmod +x php-cs-fixer
 fi
